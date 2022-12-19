bb6246f0c78e3a77440f72bc84a05f6a4983d847
Tonnie <vontonnie@gmail.com>
2022-12-19 08:14:20 +0300

    Add @GET getOdds() api function

--- app/src/main/java/com/uxstate/instantscore/data/remote/api/ScoresAPI.kt
+++ app/src/main/java/com/uxstate/instantscore/data/remote/api/ScoresAPI.kt
@@ -2,6 +2,7 @@ package com.uxstate.instantscore.data.remote.api
 
 import com.uxstate.instantscore.data.remote.dtos.match_details.MatchDetailsResponseDTO
 import com.uxstate.instantscore.data.remote.dtos.matches.MatchesResponseDTO
+import com.uxstate.instantscore.data.remote.dtos.odds.OddsResponseDTO
 import com.uxstate.instantscore.data.remote.dtos.standings.StandingResponseDTO
 import com.uxstate.instantscore.utils.*
 import retrofit2.http.GET
@@ -14,8 +15,8 @@ interface ScoresAPI {
     suspend fun getMatches(
         @Query("apikey") apiKey: String,
         @Query("season_id") seasonId: String,
-        @Query("date_from") dateFrom: String = "2022-08-06",
-        @Query("date_to") dateTo: String= "2023-05-28"
+        @Query("date_from") dateFrom: String = DATE_FROM,
+        @Query("date_to") dateTo: String = DATE_TO
     ): MatchesResponseDTO
 
     @GET(GET_MATCH_DETAILS)
@@ -29,4 +30,12 @@ interface ScoresAPI {
         @Query("apikey") apiKey: String = API_KEY_1,
         @Query("season_id") seasonId: String = DEFAULT_LEAGUE_ID
     ): StandingResponseDTO
+
+
+    @GET(GET_ODDS)
+    suspend fun getOdds(
+        @Path("match_id") matchId: String,
+        @Query("apikey") apiKey: String = API_KEY_1,
+        @Query("type") type: String = ODDS_TYPE
+    ): OddsResponseDTO
 }
--- app/src/main/java/com/uxstate/instantscore/utils/Constants.kt
+++ app/src/main/java/com/uxstate/instantscore/utils/Constants.kt
@@ -12,6 +12,10 @@ const val GET_MATCH_DETAILS = "soccer/matches/{id}"
 const val GET_STANDINGS = "soccer/standings"
 const val GET_ODDS = "/soccer/odds/{match_id}"
 
+const val ODDS_TYPE = "prematch"
+const val DATE_FROM ="2022-08-06"
+const val DATE_TO = "2023-05-28"
+
 
 
 
