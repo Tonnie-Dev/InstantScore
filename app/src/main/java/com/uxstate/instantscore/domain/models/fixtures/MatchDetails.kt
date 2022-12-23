04d13136614b87e4122f311bccd2ee075ff461f6
Tonnie <vontonnie@gmail.com>
2022-12-23 07:52:39 +0300

    Refactor Fixture model class to MatchDetails

---
+++ app/src/main/java/com/uxstate/instantscore/domain/models/fixtures/MatchDetails.kt
@@ -0,0 +1,14 @@
+package com.uxstate.instantscore.domain.models.fixtures
+
+import com.uxstate.instantscore.data.remote.dtos.top_scorers.Team
+
+data class MatchDetails(
+    val fixtureId: Int,
+    val date: Long,
+    val venue: String,
+    val status: String,
+    val timeElapsed: Int,
+    val teams: Pair<Team, Team>,
+    val events: List<Event>,
+    val stats: Stats
+)
---
+++ app/src/main/java/com/uxstate/instantscore/domain/models/fixtures/Status.kt
@@ -0,0 +1,7 @@
+package com.uxstate.instantscore.domain.models.fixtures
+
+data class Status(
+    val fixtureLong: String,
+    val fixtureShort: String,
+    val timeElapsed: Int
+)
