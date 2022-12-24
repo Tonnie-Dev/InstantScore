311c27b36ab7fe0103e045bb1f1c42d6af9e6a9f
Tonnie <vontonnie@gmail.com>
2022-12-24 18:13:02 +0300

    Add ScoresDatabase

---
+++ app/src/main/java/com/uxstate/instantscore/data/local/ScoresDatabase.kt
@@ -0,0 +1,15 @@
+package com.uxstate.instantscore.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.uxstate.instantscore.data.local.converters.Converters
+import com.uxstate.instantscore.data.local.dao.ScoresDAO
+import com.uxstate.instantscore.data.local.entities.FixtureEntity
+
+@Database(entities = [FixtureEntity::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class ScoresDatabase : RoomDatabase() {
+
+    abstract val dao: ScoresDAO
+}
