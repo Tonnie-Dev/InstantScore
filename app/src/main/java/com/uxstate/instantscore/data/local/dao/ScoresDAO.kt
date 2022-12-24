3bc8a65c616d5d85d10fb32d7151e060b7294a1e
Tonnie <vontonnie@gmail.com>
2022-12-24 18:05:00 +0300

    Add getFixtures() dao query

--- app/src/main/java/com/uxstate/instantscore/data/local/dao/ScoresDAO.kt
+++ app/src/main/java/com/uxstate/instantscore/data/local/dao/ScoresDAO.kt
@@ -3,11 +3,16 @@ package com.uxstate.instantscore.data.local.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
-import com.uxstate.instantscore.data.remote.dtos.fixture_details.Fixture
+import androidx.room.Query
+import com.uxstate.instantscore.data.local.entities.FixtureEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ScoresDAO {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertFixtures(list: List<Fixture>)
+    suspend fun insertFixtures(list: List<FixtureEntity>)
+
+    @Query("SELECT * FROM fixture_table ORDER BY date ASC")
+    fun getFixtures(): Flow<List<FixtureEntity>>
 }
