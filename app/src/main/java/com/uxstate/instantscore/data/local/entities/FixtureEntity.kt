89d6bb9124a51567754cfd351e1045e659e8232f
Tonnie <vontonnie@gmail.com>
2022-12-24 17:53:19 +0300

    Add FixtureDetailsEntity class

--- app/src/main/java/com/uxstate/instantscore/data/local/entities/FixtureEntity.kt
+++ app/src/main/java/com/uxstate/instantscore/data/local/entities/FixtureEntity.kt
@@ -1,4 +1,4 @@
-package com.uxstate.instantscore.data.local.dao
+package com.uxstate.instantscore.data.local.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
---
+++ app/src/main/java/com/uxstate/instantscore/data/local/entities/FixturesDetailsEntity.kt
@@ -0,0 +1,21 @@
+package com.uxstate.instantscore.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.uxstate.instantscore.data.remote.dtos.top_scorers.Team
+import com.uxstate.instantscore.domain.models.fixtures.Event
+import com.uxstate.instantscore.domain.models.fixtures.Stats
+
+@Entity(tableName = "fixture_details_table")
+data class FixtureDetailsEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
+    val fixtureId: Int,
+    val date: Long,
+    val venue: String,
+    val status: String,
+    val timeElapsed: Int,
+    val teams: Pair<Team, Team>,
+    val events: List<Event>,
+    val stats: Stats
+)
