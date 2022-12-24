09b165889b79549df105467f6f193d280f9615fc
Tonnie <vontonnie@gmail.com>
2022-12-24 14:52:13 +0300

    Add readStatsFromRoom() converter function

--- app/src/main/java/com/uxstate/instantscore/data/local/converters/Converters.kt
+++ app/src/main/java/com/uxstate/instantscore/data/local/converters/Converters.kt
@@ -42,15 +42,15 @@ class Converters {
     @TypeConverter
     fun writeStatsToRoom(stats: Stats): String {
 
-        val possession = stats.possession
-        val shotsOnGoal = stats.shotsOnGoal
-        val shotsOffGoal = stats.shotsOffGoal
-        val totalShots = stats.totalShots
-        val cornerKicks = stats.cornerKicks
-        val offSides = stats.offSides
-        val fouls = stats.fouls
-        val yellowCards = stats.yellowCards
-        val redCards = stats.redCards
+        val possession = stats.possession.toString()
+        val shotsOnGoal = stats.shotsOnGoal.toString()
+        val shotsOffGoal = stats.shotsOffGoal.toString()
+        val totalShots = stats.totalShots.toString()
+        val cornerKicks = stats.cornerKicks.toString()
+        val offSides = stats.offSides.toString()
+        val fouls = stats.fouls.toString()
+        val yellowCards = stats.yellowCards.toString()
+        val redCards = stats.redCards.toString()
 
         val statsPropertiesList = listOf(
                 possession,
@@ -64,7 +64,36 @@ class Converters {
                 redCards
         )
 
-      return  statsPropertiesList.joinToString("~")
+        return statsPropertiesList.joinToString("~")
+    }
+
+    @TypeConverter
+    fun readStatsFromRoom(roomString: String): Stats {
+        val statsPropertiesList = roomString.split("~")
+                .map { it }
+
+        val possession = statsPropertiesList[0].toInt()
+        val shotsOnGoal = statsPropertiesList[1].toInt()
+        val shotsOffGoal = statsPropertiesList[2].toInt()
+        val totalShots = statsPropertiesList[3].toInt()
+        val cornerKicks = statsPropertiesList[4].toInt()
+        val offSides = statsPropertiesList[5].toInt()
+        val fouls = statsPropertiesList[6].toInt()
+        val yellowCards = statsPropertiesList[7].toInt()
+        val redCards = statsPropertiesList[8].toInt()
+
+        return Stats(
+                possession = possession,
+                shotsOnGoal = shotsOnGoal,
+                shotsOffGoal = shotsOffGoal,
+                totalShots = totalShots,
+                cornerKicks = cornerKicks,
+                offSides = offSides,
+                fouls = fouls,
+                yellowCards = yellowCards,
+                redCards = redCards
+        )
+
     }
 
 
