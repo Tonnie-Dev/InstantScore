2cf7eb59b67da58bfffba0f80167460a9f289f6d
Tonnie <vontonnie@gmail.com>
2022-12-25 08:17:02 +0300

    Add Resource util class

---
+++ app/src/main/java/com/uxstate/instantscore/utils/Resource.kt
@@ -0,0 +1,18 @@
+package com.uxstate.instantscore.utils
+
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+
+    /*We attach a nullable data to the Error case as we can
+     still return some data e.g. database cache*/
+
+    class Error<T>(message: String, data: T? = null) : Resource<T>(message = message, data = data)
+
+    /*The data is null since at loading point we don't have the data*/
+    class Success<T>(data: T?) : Resource<T>(data = data)
+
+    //it has a property which will be called from ViewModel
+    class Loading<T>(val isLoading: Boolean = true) : Resource<T>()
+}
