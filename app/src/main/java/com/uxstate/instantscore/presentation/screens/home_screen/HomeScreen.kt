db8b2317d57c8fcb1778ea0afe5466e656335240
Tonnie <vontonnie@gmail.com>
2022-12-31 19:33:23 +0300

    Add League Header composable

--- app/src/main/java/com/uxstate/instantscore/presentation/screens/home_screen/HomeScreen.kt
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/home_screen/HomeScreen.kt
@@ -1,6 +1,8 @@
 package com.uxstate.instantscore.presentation.screens.home_screen
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -18,8 +20,14 @@ import com.uxstate.instantscore.presentation.screens.home_screen.events.HomeEven
 @RootNavGraph(start = true)
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
-
     val state by viewModel.fixturesState.collectAsState()
+    val map = mapOf<String, String>()
+
+    val mappedFixtures = state.fixtures.groupBy { it.league }
+    val keys = mappedFixtures.keys.map {
+        it
+    }
+    val entries = map.entries
 
     Scaffold { paddingValues ->
 
@@ -29,5 +37,11 @@ fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
                 viewModel.onEvent(event = HomeEvent.OnFixtureDateSelection(date = it))
             }
         )
+
+        LazyColumn(content = {
+
+            items(keys) {
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/home_screen/components/fixture_card/LeagueHeader.kt
@@ -0,0 +1,103 @@
+package com.uxstate.instantscore.presentation.screens.home_screen.components.fixture_card
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.uxstate.instantscore.R
+import com.uxstate.instantscore.domain.models.fixtures_schedule.League
+import com.uxstate.instantscore.presentation.ui.theme.InstantScoreTheme
+import com.uxstate.instantscore.utils.LocalSpacing
+
+@Composable
+fun LeagueHeader(league: League, modifier: Modifier = Modifier) {
+
+    val spacing = LocalSpacing.current
+    val context = LocalContext.current
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(spacing.spaceSmall)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            val painter = rememberAsyncImagePainter(
+                model = ImageRequest.Builder(context = context)
+                    .data(league.countryFlag)
+                    .error(R.drawable.empty_logo)
+                    .placeholder(R.drawable.empty_logo)
+                    .build()
+            )
+            Image(
+                painter = painter,
+                contentDescription = league.name,
+                modifier = Modifier.size(spacing.spaceSmall * 3),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceSmall))
+            Column() {
+
+                Text(text = league.name, style = MaterialTheme.typography.titleSmall)
+                Text(text = league.name, style = MaterialTheme.typography.bodySmall)
+            }
+        }
+
+        Icon(
+            imageVector = Icons.Default.KeyboardArrowRight,
+            contentDescription = "",
+            modifier = Modifier
+                .clickable { }
+                .align(Alignment.CenterEnd)
+
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LeagueHeaderPreview() {
+    InstantScoreTheme {
+
+        LeagueHeader(
+            league = League(
+                id = 13,
+                name = "Premier League",
+                country = "Kenya",
+                leagueLogo = "",
+                countryFlag = ""
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LeagueHeaderPreviewDark() {
+    InstantScoreTheme {
+
+        LeagueHeader(
+            league = League(
+                id = 7,
+                name = "E-League",
+                country = "Ethiopia",
+                leagueLogo = "",
+                countryFlag = ""
+            )
+        )
+    }
+}
