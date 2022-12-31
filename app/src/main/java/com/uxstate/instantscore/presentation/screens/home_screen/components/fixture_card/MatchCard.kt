9b035caff243184e2fd0ec5a29297e576abe057f
Tonnie <vontonnie@gmail.com>
2022-12-31 22:37:06 +0300

    Add MatchCard composable

--- app/src/main/java/com/uxstate/instantscore/presentation/screens/home_screen/components/fixture_card/MatchCard.kt
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/home_screen/components/fixture_card/MatchCard.kt
@@ -1,69 +1,100 @@
 package com.uxstate.instantscore.presentation.screens.home_screen.components.fixture_card
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.uxstate.instantscore.R
-import com.uxstate.instantscore.domain.models.fixtures_details.FixtureBonoko
-import com.uxstate.instantscore.domain.models.fixtures_schedule.Fixture
-import com.uxstate.instantscore.domain.models.fixtures_schedule.Status
-import com.uxstate.instantscore.domain.models.fixtures_schedule.Team
-import com.uxstate.instantscore.presentation.screens.common_components.HorizontalTeamLogo
+import com.uxstate.instantscore.domain.models.fixtures_schedule.*
 import com.uxstate.instantscore.presentation.ui.theme.InstantScoreTheme
 import com.uxstate.instantscore.utils.LocalSpacing
+import java.time.LocalDate
 
 @Composable
 fun MatchCard(fixture: Fixture, modifier: Modifier = Modifier) {
     val spacing = LocalSpacing.current
     val context = LocalContext.current
     val homeTeamPainter = rememberAsyncImagePainter(
-            model = ImageRequest.Builder(context = context)
-                    .data(fixture.teams.first)
-                    .error(R.drawable.empty_logo)
+        model = ImageRequest.Builder(context = context)
+            .data(fixture.teams.first)
+            .error(R.drawable.empty_flag)
+            .placeholder(R.drawable.empty_flag)
+            .crossfade(true)
+            .build()
     )
 
-    Card(modifier = modifier) {
+    val awayTeamPainter = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(context = context)
+            .data(fixture.teams.second)
+            .error(R.drawable.empty_flag)
+            .placeholder(R.drawable.empty_flag)
+            .crossfade(true)
+            .build()
+    )
 
+    Card(modifier = modifier, shape = RectangleShape) {
 
-        Row() {
-            Text(text = fixture.status.fixtureShort)
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = fixture.status.fixtureShort,
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
 
             Column() {
-                Row() {
+                Row(
+                    modifier = Modifier.fillMaxWidth(.75f),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Image(
+                            painter = homeTeamPainter,
+                            contentDescription = fixture.teams.first.name,
+                            Modifier.size(spacing.spaceExtraSmall * 4)
+                        )
+                        Spacer(modifier = Modifier.width(spacing.spaceSmall))
+                        Text(text = fixture.teams.first.name)
+                    }
 
+                    Text(
+                        text = fixture.goals.homeTeamScore.toString(),
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(spacing.spaceSmall))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(.75f),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Image(
+                            painter = homeTeamPainter,
+                            contentDescription = fixture.teams.first.name,
+                            Modifier.size(spacing.spaceExtraSmall * 4)
+                        )
+                        Spacer(modifier = Modifier.width(spacing.spaceSmall))
+                        Text(text = fixture.teams.second.name)
+                    }
+
+                    Text(
+                        text = fixture.goals.awayTeamScore.toString(),
+                        fontWeight = FontWeight.Bold
+                    )
                 }
             }
-        }
-        Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceAround,
-                modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(spacing.spaceSmall)
-        ) {
-            HorizontalTeamLogo(team = fixture.teams.first)
-            Surface(
-
-                    border = BorderStroke(
-                            width = spacing.spaceHalfDp,
-                            color = MaterialTheme.colorScheme.onSurface
-                    ),
-                    modifier = Modifier.padding(spacing.spaceExtraSmall)
-            ) {
-                Text(text = fixture.date.toString())
-            }
-            HorizontalTeamLogo(team = fixture.teams.second)
         }
     }
 }
@@ -72,21 +103,30 @@ fun MatchCard(fixture: Fixture, modifier: Modifier = Modifier) {
 @Composable
 fun MatchCardPreview() {
 
-    MatchCard(
-            fixture = FixtureBonoko(
-                    fixtureId = 0,
-                    date = 1000,
-                    status = Status(
-                            fixtureLong = "",
-                            fixtureShort = "",
-                            timeElapsed = 0
-                    ),
-                    teams = Pair(
-                            Team(name = "Brentford", logo = ""),
-                            Team(name = "Arsenal", logo = "")
-                    )
+    InstantScoreTheme() {
+        MatchCard(
+            fixture = Fixture(
+                fixtureId = 0, date = LocalDate.now(),
+                status = Status(
+                    fixtureLong = "",
+                    fixtureShort = "FT",
+                    timeElapsed = 63
+                ),
+                league = League(
+                    id = 0,
+                    name = "China League",
+                    country = "",
+                    leagueLogo = "",
+                    countryFlag = ""
+                ),
+                teams = Pair(Team(name = "Auxere", logo = ""), Team(name = "Monaco", logo = "")),
+                goals = Goal(
+                    homeTeamScore = 5,
+                    awayTeamScore = 3
+                )
             )
-    )
+        )
+    }
 }
 
 @Preview(uiMode = UI_MODE_NIGHT_YES)
@@ -94,20 +134,29 @@ fun MatchCardPreview() {
 fun MatchCardPreviewDark() {
 
     InstantScoreTheme() {
-        MatchCard(
-                fixture = FixtureBonoko(
-                        fixtureId = 0,
-                        date = 1000,
-                        status = Status(
-                                fixtureLong = "",
-                                fixtureShort = "",
-                                timeElapsed = 0
-                        ),
-                        teams = Pair(
-                                Team(name = "Brentford", logo = ""),
-                                Team(name = "Arsenal", logo = "")
-                        )
+        InstantScoreTheme() {
+            MatchCard(
+                fixture = Fixture(
+                    fixtureId = 0, date = LocalDate.now(),
+                    status = Status(
+                        fixtureLong = "",
+                        fixtureShort = "FT",
+                        timeElapsed = 63
+                    ),
+                    league = League(
+                        id = 0,
+                        name = "China League",
+                        country = "",
+                        leagueLogo = "",
+                        countryFlag = ""
+                    ),
+                    teams = Pair(Team(name = "Brest", logo = ""), Team(name = "Lyon", logo = "")),
+                    goals = Goal(
+                        homeTeamScore = 5,
+                        awayTeamScore = 3
+                    )
                 )
-        )
+            )
+        }
     }
 }
