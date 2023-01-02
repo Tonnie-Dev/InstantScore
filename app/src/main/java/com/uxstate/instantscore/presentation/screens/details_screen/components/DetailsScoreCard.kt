954b3c8be83ccbb50efc7883a5f43281925b6156
Tonnie <vontonnie@gmail.com>
2023-01-02 19:39:01 +0300

    Add FixiturePoster composable file

--- app/src/main/java/com/uxstate/instantscore/presentation/screens/details_screen/components/DetailsScoreCard.kt
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/details_screen/components/DetailsScoreCard.kt
@@ -1,5 +1,6 @@
 package com.uxstate.instantscore.presentation.screens.details_screen.components
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
@@ -7,6 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import coil.compose.rememberAsyncImagePainter
@@ -18,7 +20,7 @@ import com.uxstate.instantscore.utils.LocalSpacing
 import java.time.LocalDate
 
 @Composable
-fun DetailsScoreCard(fixture: Fixture) {
+fun DetailsScoreCard(fixture: Fixture, modifier: Modifier = Modifier) {
     val spacing = LocalSpacing.current
     val context = LocalContext.current
 
@@ -45,7 +47,8 @@ fun DetailsScoreCard(fixture: Fixture) {
     )
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
     ) {
 
         // Home Team
@@ -57,17 +60,30 @@ fun DetailsScoreCard(fixture: Fixture) {
             Image(
                 painter = homeTeamPainter,
                 contentDescription = fixture.teams.first.name,
-                Modifier.size(spacing.spaceExtraLarge).padding(spacing.spaceSmall)
+                Modifier
+                    .size(spacing.spaceExtraLarge)
+                    .padding(spacing.spaceSmall)
             )
 
-            Text(text = fixture.teams.first.name, style = MaterialTheme.typography.titleMedium)
+            Text(
+                text = fixture.teams.first.name, style = MaterialTheme.typography.displaySmall,
+                color = Color.White
+            )
         }
 
         // Scores Row
         Row {
-            Text(text = homeGoals, style = MaterialTheme.typography.displaySmall)
-            Text(text = "-", style = MaterialTheme.typography.displaySmall)
-            Text(text = awayGoals, style = MaterialTheme.typography.displaySmall)
+            Text(
+                text = homeGoals,
+                style = MaterialTheme.typography.displaySmall,
+                color = Color.White
+            )
+            Text(text = "-", style = MaterialTheme.typography.displaySmall, color = Color.White)
+            Text(
+                text = awayGoals,
+                style = MaterialTheme.typography.displaySmall,
+                color = Color.White
+            )
         }
 
         // Away Team
@@ -77,12 +93,15 @@ fun DetailsScoreCard(fixture: Fixture) {
         ) {
 
             Image(
-                painter = homeTeamPainter,
+                painter = awayTeamPainter,
                 contentDescription = fixture.teams.first.name,
                 Modifier.size(spacing.spaceExtraLarge)
             )
 
-            Text(text = fixture.teams.first.name, style = MaterialTheme.typography.titleMedium)
+            Text(
+                text = fixture.teams.first.name,
+                style = MaterialTheme.typography.displaySmall, color = Color.White
+            )
         }
     }
 }
@@ -90,6 +109,39 @@ fun DetailsScoreCard(fixture: Fixture) {
 @Preview
 @Composable
 fun DetailsScoreCardPreview() {
+    InstantScoreTheme() {
+        DetailsScoreCard(
+            fixture = Fixture(
+                fixtureId = 0, date = LocalDate.now(),
+                status = Status(
+                    fixtureLong = "",
+                    fixtureShort = "FT",
+                    timeElapsed = 63
+                ),
+                league = League(
+                    id = 0,
+                    name = "China League",
+                    country = "",
+                    leagueLogo = "",
+                    countryFlag = ""
+                ),
+                teams = Pair(
+                    Team(name = "Auxere", logo = ""),
+                    Team(name = "Monaco", logo = "")
+                ),
+                goals = Goal(
+                    homeTeamScore = 5,
+                    awayTeamScore = 3
+                ),
+                startTime = 1672774200
+            )
+        )
+    }
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun DetailsScoreCardPreviewDark() {
     InstantScoreTheme() {
         DetailsScoreCard(
             fixture = Fixture(
--- app/src/main/java/com/uxstate/instantscore/presentation/screens/details_screen/components/FixturePoster.kt
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/details_screen/components/FixturePoster.kt
@@ -1,11 +1,112 @@
 package com.uxstate.instantscore.presentation.screens.details_screen
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.uxstate.instantscore.R
+import com.uxstate.instantscore.domain.models.fixtures_schedule.*
+import com.uxstate.instantscore.presentation.screens.details_screen.components.DetailsScoreCard
+import com.uxstate.instantscore.presentation.ui.theme.InstantScoreTheme
+import java.time.LocalDate
 
 @Composable
-fun FixturePoster() {
+fun FixturePoster(fixture: Fixture) {
 
-    Box(contentAlignment = Alignment.TopCenter) {}
+    Box(contentAlignment = Alignment.TopCenter) {
+
+        Image(
+            painter = painterResource(R.drawable.fixture_background_one),
+            contentDescription = stringResource(
+                R.string.stadium_background
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(2f / 1f)
+        )
+
+        Text(
+            text = fixture.league.name,
+            style = MaterialTheme.typography.displaySmall,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.TopCenter)
+        )
+        DetailsScoreCard(fixture = fixture, modifier = Modifier.align(Alignment.Center))
+    }
+}
+
+@Preview
+@Composable
+fun FixturePosterPreview() {
+    InstantScoreTheme() {
+        FixturePoster(
+            fixture = Fixture(
+                fixtureId = 0, date = LocalDate.now(),
+                status = Status(
+                    fixtureLong = "",
+                    fixtureShort = "FT",
+                    timeElapsed = 63
+                ),
+                league = League(
+                    id = 0,
+                    name = "China League",
+                    country = "",
+                    leagueLogo = "",
+                    countryFlag = ""
+                ),
+                teams = Pair(
+                    Team(name = "Auxere", logo = ""),
+                    Team(name = "Monaco", logo = "")
+                ),
+                goals = Goal(
+                    homeTeamScore = 5,
+                    awayTeamScore = 3
+                ),
+                startTime = 1672774200
+            )
+        )
+    }
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun FixturePosterPreviewDark() {
+    InstantScoreTheme() {
+        FixturePoster(
+            fixture = Fixture(
+                fixtureId = 0, date = LocalDate.now(),
+                status = Status(
+                    fixtureLong = "",
+                    fixtureShort = "FT",
+                    timeElapsed = 63
+                ),
+                league = League(
+                    id = 0,
+                    name = "China League",
+                    country = "",
+                    leagueLogo = "",
+                    countryFlag = ""
+                ),
+                teams = Pair(
+                    Team(name = "Auxere", logo = ""),
+                    Team(name = "Monaco", logo = "")
+                ),
+                goals = Goal(
+                    homeTeamScore = 5,
+                    awayTeamScore = 3
+                ),
+                startTime = 1672774200
+            )
+        )
+    }
 }
