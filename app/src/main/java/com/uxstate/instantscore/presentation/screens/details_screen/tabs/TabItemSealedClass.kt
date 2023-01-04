3745f31e30879bef7a5f18e859cfe19d0a7212df
Tonnie <vontonnie@gmail.com>
2023-01-04 07:34:34 +0300

    Add TabItemSealed class

---
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/details_screen/tabs/TabItemSealedClass.kt
@@ -0,0 +1,10 @@
+package com.uxstate.instantscore.presentation.screens.details_screen.tabs
+
+import androidx.compose.runtime.Composable
+
+sealed class TabItemSealedClass(val tabTitle: String, val composeFunction: @Composable () -> Unit) {
+
+    object EventsTab : TabItemSealedClass(tabTitle = "Event", composeFunction = {})
+    object LineUpsTab : TabItemSealedClass(tabTitle = "Lineups", composeFunction = {})
+    object StatsTab : TabItemSealedClass(tabTitle = "Stats", composeFunction = {})
+}
