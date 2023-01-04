d9a7e9e162be032e16b2d3f37c5fbd5910ba1653
Tonnie <vontonnie@gmail.com>
2023-01-04 17:57:07 +0300

    Add Coach parcelable model class

---
+++ app/src/main/java/com/uxstate/instantscore/domain/models/fixture_details/Coach.kt
@@ -0,0 +1,11 @@
+package com.uxstate.instantscore.domain.models.fixture_details
+
+import android.os.Parcelable
+
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Coach(
+    val name: String,
+    val photo: String
+) : Parcelable
