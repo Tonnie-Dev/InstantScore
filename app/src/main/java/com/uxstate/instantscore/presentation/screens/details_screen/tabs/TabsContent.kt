aa63358f63f7b4b9edc98f3c32b38be25c095040
Tonnie <vontonnie@gmail.com>
2023-01-04 08:44:39 +0300

    Add TabsContent composable

---
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/details_screen/tabs/TabsContent.kt
@@ -0,0 +1,18 @@
+package com.uxstate.instantscore.presentation.screens.details_screen.tabs
+
+import androidx.compose.runtime.Composable
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabsContent(tabs:List<TabItemSealedClass>, pagerState: PagerState) {
+
+    HorizontalPager(count = tabs.size, state = pagerState) {
+        page: Int ->
+
+        tabs[page].composeFunction
+    }
+
+}
