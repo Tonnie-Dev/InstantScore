e461a9c33008c98668e5f871ade98f291c0a78d7
Tonnie <vontonnie@gmail.com>
2023-01-11 20:41:43 +0300

    Delete Logs from EventsBoard file

--- app/src/main/java/com/uxstate/instantscore/presentation/screens/details_screen/components/events/EventsBoard.kt
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/details_screen/components/events/EventsBoard.kt
@@ -3,7 +3,6 @@ package com.uxstate.instantscore.presentation.screens.details_screen.components.
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.uxstate.instantscore.domain.models.fixture_details.FixtureDetails
@@ -19,8 +18,6 @@ fun EventsBoard(fixtureDetails: FixtureDetails, modifier: Modifier = Modifier) {
     Timber.i("The events are: $events")
 
     LazyColumn(modifier = modifier.fillMaxWidth()) {
-
-        Timber.i("Entering lazy column")
 
         items(events) { event ->
 
@@ -41,12 +38,6 @@ fun EventsBoard(fixtureDetails: FixtureDetails, modifier: Modifier = Modifier) {
                 "subst" -> {
                     Timber.i("Sub Event!")
                     SubstitutionEvent(event = event, isHomeTeamEvent = isHomeEvent)
-                }
-
-                else -> {
-
-                    Timber.i("Or-Else Event!!!")
-                    Text(text = "Nothing")
                 }
             }
         }
