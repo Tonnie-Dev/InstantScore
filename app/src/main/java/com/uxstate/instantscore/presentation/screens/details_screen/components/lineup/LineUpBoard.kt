bd2f3f92a8afa8d753b3a64d6e7b85ea79e58dd6
Tonnie <vontonnie@gmail.com>
2023-01-12 08:59:35 +0300

    Modify PlayerCard composable

---
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/details_screen/components/lineup/LineUpBoard.kt
@@ -0,0 +1,7 @@
+package com.uxstate.instantscore.presentation.screens.details_screen.components.lineup
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LineUpBoard() {
+}
--- app/src/main/java/com/uxstate/instantscore/presentation/screens/details_screen/components/lineup/PlayerCard.kt
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/details_screen/components/lineup/PlayerCard.kt
@@ -1,12 +1,7 @@
 package com.uxstate.instantscore.presentation.screens.details_screen.components.lineup
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,8 +9,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import com.uxstate.instantscore.R
 import com.uxstate.instantscore.domain.models.fixture_details.Player
 import com.uxstate.instantscore.presentation.ui.theme.InstantScoreTheme
 import com.uxstate.instantscore.utils.LocalSpacing
@@ -25,27 +22,33 @@ fun PlayerCard(player: Player, modifier: Modifier = Modifier, teamColor: Color =
     val spacing = LocalSpacing.current
 
     Row(
-            horizontalArrangement = Arrangement.spacedBy(spacing.spaceMedium),
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(spacing.spaceExtraSmall)
+        horizontalArrangement = Arrangement.spacedBy(spacing.spaceMedium),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(spacing.spaceExtraSmall)
     ) {
-        Icon(
-                imageVector = Icons.Default.Person,
+
+        Box() {
+            Icon(
+                painter = painterResource(id = R.drawable.shirt_black),
                 contentDescription = player.playerName,
-                modifier = modifier.size(spacing.spaceMedium),
+                modifier = modifier.size(spacing.spaceLarge),
                 tint = teamColor
-        )
+            )
 
-        Text(
-                text = player.playerName,
-                fontWeight = FontWeight.SemiBold,
-                style = MaterialTheme.typography.titleSmall
-        )
-
-        Text(
+            Text(
                 text = player.playerNumber.toString(),
                 fontWeight = FontWeight.SemiBold,
-                style = MaterialTheme.typography.titleSmall
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(spacing.spaceExtraSmall)
+            )
+        }
+
+        Text(
+            text = player.playerName,
+            fontWeight = FontWeight.SemiBold,
+            style = MaterialTheme.typography.titleSmall
         )
     }
 }
@@ -60,7 +63,6 @@ fun PlayerCardPreview() {
     InstantScoreTheme() {
         PlayerCard(player = player, teamColor = Color.Blue)
     }
-
 }
 
 @Preview(uiMode = UI_MODE_NIGHT_YES)
@@ -71,7 +73,6 @@ fun PlayerCardPreviewDark() {
         Player(playerId = 0, playerName = "Tonnie Von", playerNumber = 13, playerPosition = "M")
 
     InstantScoreTheme() {
-        PlayerCard(player = player, teamColor = Color.Blue)
+        PlayerCard(player = player, teamColor = Color.White)
     }
-
 }
