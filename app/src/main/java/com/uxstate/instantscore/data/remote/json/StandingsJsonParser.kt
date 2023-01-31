97a77d93a54bcb2832a3bc9ae4d4b90cc3953e3f
Tonnie <vontonnie@gmail.com>
2023-01-31 17:10:33 +0300

    Add League model class for statndings package

--- app/src/main/java/com/uxstate/instantscore/data/remote/json/StandingsJsonParser.kt
+++ app/src/main/java/com/uxstate/instantscore/data/remote/json/StandingsJsonParser.kt
@@ -1,9 +1,17 @@
 package com.uxstate.instantscore.data.remote.json
 
 import com.uxstate.instantscore.domain.models.standings.Standing
+import org.json.JSONObject
 
 class StandingsJsonParser:JsonStringParser<Standing> {
     override fun parseJsonToFixtureDetails(jsonString: String): Standing {
+
+        val mainJsonStandingsResponseObj = JSONObject(jsonString)
+
+val responseJsonArray = mainJsonStandingsResponseObj.getJSONArray("response")
+        val responseInnerObj = responseJsonArray.getJSONObject(0)
+        val leagueObj = responseInnerObj.getJSONObject("league")
+
         TODO("Not yet implemented")
     }
 
---
+++ app/src/main/java/com/uxstate/instantscore/domain/models/standings/League.kt
@@ -0,0 +1,13 @@
+package com.uxstate.instantscore.domain.models.standings
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class League(
+    val id: Int,
+    val name: String,
+    val country: String,
+    val leagueLogo: String,
+    val countryFlag: String
+) : Parcelable
