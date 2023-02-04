bd2060485a5206c03578e9c46b37111ba3c1da3f
Tonnie <vontonnie@gmail.com>
2023-02-04 08:49:25 +0300

    Add StandingsViewModel class

--- app/src/main/java/com/uxstate/instantscore/presentation/screens/standings_screen/StandingsViewModel.kt
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/standings_screen/StandingsViewModel.kt
@@ -2,12 +2,17 @@ package com.uxstate.instantscore.presentation.screens.standings_screen
 
 import androidx.lifecycle.ViewModel
 import com.uxstate.instantscore.domain.usecases.UseCaseContainer
+import com.uxstate.instantscore.presentation.screens.standings_screen.state.StandingsState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class StandingsViewModel @Inject constructor(private val useCaseContainer: UseCaseContainer) :
     ViewModel() {
 
-        
+    private val _standingsState = MutableStateFlow(StandingsState())
+    val standingsState = _standingsState.asStateFlow()
+
 }
