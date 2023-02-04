47c780f3fc63722a836f5cccf68b72f4f2d971e0
Tonnie <vontonnie@gmail.com>
2023-02-04 20:04:42 +0300

    Add sendUIEvent() StandingsViewModel function

--- app/src/main/java/com/uxstate/instantscore/domain/usecases/GetStandingsUseCase.kt
+++ app/src/main/java/com/uxstate/instantscore/domain/usecases/GetStandingsUseCase.kt
@@ -4,11 +4,13 @@ import com.uxstate.instantscore.domain.models.standings.Standing
 import com.uxstate.instantscore.domain.repository.ScoresRepository
 import com.uxstate.instantscore.utils.Resource
 import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
 
 class GetStandingsUseCase(private val repository: ScoresRepository) {
 
-    operator fun invoke(leagueId: Int, season: Int): Flow<Resource<MutableList<Standing>>> {
+    operator fun invoke(leagueId: Int): Flow<Resource<MutableList<Standing>>> {
 
+        val season = LocalDate.now().year
         return repository.getStandings(leagueId = leagueId, season = season)
     }
 }
--- app/src/main/java/com/uxstate/instantscore/presentation/screens/standings_screen/StandingsViewModel.kt
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/standings_screen/StandingsViewModel.kt
@@ -2,13 +2,20 @@ package com.uxstate.instantscore.presentation.screens.standings_screen
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.uxstate.instantscore.domain.usecases.UseCaseContainer
 import com.uxstate.instantscore.presentation.screens.navArgs
 import com.uxstate.instantscore.presentation.screens.standings_screen.state.StandingsState
+import com.uxstate.instantscore.utils.Resource
+import com.uxstate.instantscore.utils.UIEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
 class StandingsViewModel @Inject constructor(
@@ -20,6 +27,40 @@ class StandingsViewModel @Inject constructor(
     private val _standingsState = MutableStateFlow(StandingsState())
     val standingsState = _standingsState.asStateFlow()
 
+    private val _uiEvent = Channel<UIEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
     // use generated extension function on the handle
     val leagueId = handle.navArgs<LeagueNavArgumentsHolder>().id
+
+    private fun getStandings(leagueId: Int) {
+
+        useCaseContainer.getStandingsUseCase(leagueId)
+                .onEach { result ->
+                    when (result) {
+
+                        is Resource.Success -> {
+
+                            result.data?.let {
+                                _standingsState.value =
+                                    _standingsState.value.copy(standingsList = it)
+                            }
+                        }
+                        is Resource.Error -> {
+
+
+                        }
+                        is Resource.Loading -> {}
+                    }
+                }
+    }
+
+
+    private fun sendUIEvent(uiEvent:UIEvent){
+
+        viewModelScope.launch {
+
+            _uiEvent.send(uiEvent)
+        }
+    }
 }
