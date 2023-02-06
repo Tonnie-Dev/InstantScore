6681043c0850fedc81debec76d6dc9da3571029f
Tonnie <vontonnie@gmail.com>
2023-02-06 20:13:02 +0300

    Add LiveFixturesList composable file

--- app/src/main/java/com/uxstate/instantscore/presentation/screens/live_screen/LiveFixturesScreen.kt
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/live_screen/LiveFixturesScreen.kt
@@ -1,2 +1,10 @@
 package com.uxstate.instantscore.presentation.screens.live_screen
 
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun LiveFixturesScreen(viewModel: LiveFixturesViewModel = hiltViewModel()) {
+
+    val state = viewModel.liveFixturesState
+}
---
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/live_screen/components/FixturesColumn.kt
@@ -0,0 +1,65 @@
+package com.uxstate.instantscore.presentation.screens.live_screen.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.PullRefreshState
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.zIndex
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.uxstate.instantscore.domain.models.fixtures_schedule.Fixture
+import com.uxstate.instantscore.domain.models.fixtures_schedule.League
+import com.uxstate.instantscore.presentation.screens.destinations.DetailsScreenDestination
+import com.uxstate.instantscore.presentation.screens.home_screen.components.fixture_card.LeagueFixturesCard
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun LiveFixturesList(
+    mappedFixtures: List<Pair<League, List<Fixture>>>,
+    navigator: DestinationsNavigator,
+    pullRefreshState: PullRefreshState,
+    isLoading: Boolean,
+    isRefreshing: Boolean
+) {
+
+    Box(
+        modifier = Modifier
+            .pullRefresh(state = pullRefreshState)
+            .zIndex(-1f)
+
+    ) {
+
+        if (isLoading)
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        else {
+            // content to be refreshed
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+
+                items(mappedFixtures) {
+
+                    LeagueFixturesCard(
+                        league = it.first,
+                        fixtures = it.second,
+                        onClickFixtureCard = { fixtureId ->
+                            navigator.navigate(DetailsScreenDestination(fixtureId))
+                        }, onClickLeagueHeader = {}
+                    )
+                }
+            }
+
+            PullRefreshIndicator(
+                // controls the infinite spinning
+                refreshing = isRefreshing,
+                state = pullRefreshState,
+                modifier = Modifier.align(Alignment.TopCenter)
+            )
+        }
+    }
+}
