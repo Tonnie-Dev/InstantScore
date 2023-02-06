cff545e46d16555e614bddce830661e0f4ebdfa1
Tonnie <vontonnie@gmail.com>
2023-02-06 20:39:14 +0300

    Add LaunchedEffect for LiveFixturesScreen

--- app/src/main/java/com/uxstate/instantscore/presentation/screens/live_screen/components/LiveFixturesHeader.kt
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/live_screen/components/LiveFixturesHeader.kt
@@ -3,9 +3,11 @@ package com.uxstate.instantscore.presentation.screens.live_screen.components
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,13 +16,15 @@ import com.uxstate.instantscore.presentation.ui.theme.InstantScoreTheme
 
 @Composable
 fun LiveFixturesHeader(modifier: Modifier = Modifier) {
-
+Card(shape = RectangleShape) {
     Text(
-        text = stringResource(R.string.live_fixtures_text),
-        style = MaterialTheme.typography.displaySmall,
-        modifier = modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center
+            text = stringResource(R.string.live_fixtures_text),
+            style = MaterialTheme.typography.displaySmall,
+            modifier = modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
     )
+}
+
 }
 
 @Preview
