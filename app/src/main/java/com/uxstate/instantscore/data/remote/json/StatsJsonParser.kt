2f22f58468a4bf083ffbed8ee8cfe0ffdae51d1b
Tonnie <vontonnie@gmail.com>
2023-02-09 21:23:11 +0300

    Add the first few lines for StatsJsonParser

--- app/src/main/java/com/uxstate/instantscore/data/remote/json/StatsJsonParser.kt
+++ app/src/main/java/com/uxstate/instantscore/data/remote/json/StatsJsonParser.kt
@@ -1,10 +1,52 @@
 package com.uxstate.instantscore.data.remote.json
 
 import com.uxstate.instantscore.domain.models.player_stats.PlayerStats
+import org.json.JSONObject
 import javax.inject.Inject
 
-class StatsJsonParser@Inject constructor():JsonStringParser<MutableList<PlayerStats>> {
+class StatsJsonParser @Inject constructor() : JsonStringParser<MutableList<PlayerStats>> {
     override fun parsJsonString(jsonString: String): MutableList<PlayerStats> {
+        //to get the mainJsonObject pass the raw JSON String from the API
+
+        val mainJsonObject = JSONObject(jsonString)
+
+        //with the main Json object get the response array passing "response" as the key
+
+        val responseJsonArray = mainJsonObject.getJSONArray("response")
+
+
+        //at this point you want to iterate the contents of the responseJsonArray - use
+        //until ArrayOutOfIndex exception
+
+        //initilize an empty mutable list just outside the forEach loop
+
+        val playerStatsList = mutableListOf<PlayerStats>()
+        (0 until responseJsonArray.length()).forEach { i ->
+
+            //use the position of the object inside the JsonArray to get the current object
+            //during iteration
+
+            val innerPlayerObj = responseJsonArray.getJSONObject(i)
+
+            //use "player" as the key to get the player Json Object
+            val playerJsonObj = innerPlayerObj.getJSONObject("player")
+
+            //variables - name, playerPhoto
+            val playerName = playerJsonObj.optString("name", "")
+            val playerPhoto = playerJsonObj.optString("photo")
+
+
+            val statisticsJsonArray = innerPlayerObj.getJSONArray("statistics")
+
+            val innerStatisticsJsonObj = statisticsJsonArray.getJSONObject(0)
+
+            val teamObj = innerStatisticsJsonObj.getJSONObject("team")
+
+            //variables teamName, teamLogo
+            val teamName = teamObj.optString("name", "")
+            val teamLogo = teamObj.optString("logo", "")
+        }
+
         TODO("Not yet implemented")
     }
 }
