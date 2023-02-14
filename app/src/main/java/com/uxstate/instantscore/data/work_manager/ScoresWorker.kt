a5e08cfe465b652b4ee04376ce8a573453db6441
Tonnie <vontonnie@gmail.com>
2023-02-14 08:28:46 +0300

    Add ScoresWorker companion object

--- app/src/main/java/com/uxstate/instantscore/data/work_manager/ScoresWorker.kt
+++ app/src/main/java/com/uxstate/instantscore/data/work_manager/ScoresWorker.kt
@@ -1,14 +1,36 @@
 package com.uxstate.instantscore.data.work_manager
 
 import android.content.Context
-import androidx.work.CoroutineWorker
-import androidx.work.WorkerParameters
+import androidx.work.*
+import com.uxstate.instantscore.data.local.ScoresDatabase
+import com.uxstate.instantscore.data.remote.api.ScoresAPI
+import java.util.concurrent.TimeUnit
 
 class ScoresWorker(
     private val context: Context,
-    private val params: WorkerParameters
+    private val params: WorkerParameters,
+    private val db: ScoresDatabase,
+    private val api: ScoresAPI
 ) : CoroutineWorker(context, params) {
     override suspend fun doWork(): Result {
         TODO("Not yet implemented")
+    }
+
+    companion object {
+
+        private const val SCORES_WORKER_ID = "scores_worker"
+        fun schedule(context: Context) {
+
+            val constraints = Constraints.Builder()
+                    .setRequiredNetworkType(NetworkType.CONNECTED)
+                    .setRequiresStorageNotLow(true)
+                    .build()
+
+            val request =
+                PeriodicWorkRequestBuilder<ScoresWorker>(
+                        6, TimeUnit.HOURS,
+                        1, TimeUnit.HOURS
+                )
+        }
     }
 }
