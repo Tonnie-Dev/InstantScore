f696882bafc196a3c4a78ebe281a063dd9df6eb1
Tonnie <vontonnie@gmail.com>
2023-02-15 20:43:01 +0300

    Add WorkManagerInitializerModule object

---
+++ app/src/main/java/com/uxstate/instantscore/di/WorkManagerInitializerModule.kt
@@ -0,0 +1,34 @@
+package com.uxstate.instantscore.di
+
+import android.content.Context
+import androidx.startup.Initializer
+import androidx.work.Configuration
+import androidx.work.WorkManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import timber.log.Timber
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+
+
+object WorkManagerInitializerModule:Initializer<WorkManager> {
+
+    @Provides
+    @Singleton
+    override fun create(context: Context): WorkManager {
+
+        val configuration  = Configuration.Builder().build()
+        WorkManager.initialize(context, configuration)
+
+        Timber.i("WorkManager initialized by Hilt")
+        return WorkManager.getInstance(context)
+    }
+
+    override fun dependencies(): MutableList<Class<out Initializer<*>>> {
+        return mutableListOf()
+    }
+}
