8504343caedfbe1a12763fa820ee8da7cce0cbdb
Tonnie <vontonnie@gmail.com>
2023-02-15 19:33:51 +0300

    Override getWorkManagerConfiguration in the application class

--- app/src/main/java/com/uxstate/instantscore/InstantScoreApp.kt
+++ app/src/main/java/com/uxstate/instantscore/InstantScoreApp.kt
@@ -1,18 +1,25 @@
 package com.uxstate.instantscore
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
-class InstantScoreApp : Application(), Configuration.Provider{
+class InstantScoreApp : Application(), Configuration.Provider {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
     }
 
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
     override fun getWorkManagerConfiguration(): Configuration {
-        TODO("Not yet implemented")
+        return Configuration.Builder()
+                .setWorkerFactory(workerFactory)
+                .build()
     }
 }
