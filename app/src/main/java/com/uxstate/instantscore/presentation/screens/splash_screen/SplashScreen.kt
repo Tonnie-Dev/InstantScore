9a392ccb442ec2f4008e0942b93356a1fa04cfd1
Tonnie <vontonnie@gmail.com>
2023-02-25 20:56:07 +0300

    Add Splash screen composable file

---
+++ app/src/main/java/com/uxstate/instantscore/presentation/screens/splash_screen/SplashScreen.kt
@@ -0,0 +1,43 @@
+package com.uxstate.instantscore.presentation.screens.splash_screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+
+
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.uxstate.instantscore.utils.LocalSpacing
+import com.uxstate.instantscore.R
+import com.uxstate.instantscore.presentation.screens.destinations.HomeScreenDestination
+
+@Composable
+fun SplashScreen(navigator: DestinationsNavigator) {
+    
+    val spacing = LocalSpacing.current
+    val spec = LottieCompositionSpec.RawRes(R.raw.soccer_splash)
+    val composition by rememberLottieComposition(spec = spec)
+    val state = animateLottieCompositionAsState(composition = composition)
+    
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+
+        LottieAnimation(
+                composition = composition, progress = { state.progress },
+
+        )
+
+
+        if (state.isAtEnd && state.isPlaying) {
+
+            navigator.navigate(HomeScreenDestination)
+        }
+
+    }
+}
