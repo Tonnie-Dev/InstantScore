f4ee641889a2dabacba76acc3821ddb82e515777
Tonnie <vontonnie@gmail.com>
2023-03-10 20:45:58 +0300

    Remove Extra Constants

--- app/src/main/java/com/uxstate/instantscore/utils/Constants.kt
+++ app/src/main/java/com/uxstate/instantscore/utils/Constants.kt
@@ -1,11 +1,7 @@
 package com.uxstate.instantscore.utils
 
 const val BASE_URL = "https://api-football-v1.p.rapidapi.com/"
-const val FAKE_BASE_URL_1 = "https://47038c84-3822-4df0-9583-2c8d89c9a7b9.mock.pstmn.io/"
-const val FAKE_BASE_URL_2 = "https://24a34667-9952-4941-b18b-3735ea2c9843.mock.pstmn.io/"
-const val FAKE_BASE_URL_3 = "https://f6751646-8213-41ea-89a9-84a1811b408f.mock.pstmn.io/"
-const val FAKE_BASE_URL_4 = "https://dec47f41-c146-4c6a-b6d3-94d7dd8e8f30.mock.pstmn.io/"
-const val FAKE_BASE_URL_5 = "https://d3fe86f6-e599-4f62-bc9f-bc4b96296bad.mock.pstmn.io/"
+
 
 // Headers
 const val API_KEY = "ac94f94bb2msh209b8322dafe16ep19e183jsnabc6710bd5a9"
