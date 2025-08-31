9c598b38f6cf3e1ce9d13cdb2729a9c30a8d6702
Tonnie <vontonnie@gmail.com>
2025-08-31 19:59:04 +0300

    Update library versions and minor code cleanup

--- app/src/main/java/com/uxstate/instantscore/InstantScoreApp.kt
+++ app/src/main/java/com/uxstate/instantscore/InstantScoreApp.kt
@@ -24,7 +24,6 @@ class InstantScoreApp : Application(), Configuration.Provider {
         get() = Configuration.Builder()
                 .setWorkerFactory(workerFactory)
                 .build()
-
     }
 
 
